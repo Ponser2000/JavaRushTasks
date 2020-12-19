3c4e5adb303cc5be9465198833b4938df2537016
Ponser2000 <ponser2000@gmail.com>
2020-12-19 19:14:27 +0300

    - 2048 (10)

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Main.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Main.java
@@ -1,11 +1,29 @@
 package com.javarush.task.task35.task3513;
 
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
 /**
  * @author Sergey Ponomarev on 19.12.2020
  * @project JavaRushTasks/com.javarush.task.task35.task3513
  */
 public class Main {
   
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    Model model = new Model();
+    Controller controller = new Controller(model);
+    JFrame jFrame = new JFrame();
+
+    jFrame.setTitle("2048");
+    jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    jFrame.setSize(450, 500);
+    jFrame.setResizable(false);
+
+    jFrame.add(controller.getView());
+
+
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
 
 }
