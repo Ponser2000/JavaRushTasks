5e5b1e02313d1be98d888b728d7389688bc03d34
Ponser2000 <ponser2000@gmail.com>
2020-12-19 19:00:28 +0300

    - 2048 (5)

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,7 +1,10 @@
 package com.javarush.task.task35.task3513;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * @author Sergey Ponomarev on 19.12.2020
@@ -12,6 +15,9 @@ public class Model {
   private static final int FIELD_WIDTH = 4;
 
   private Tile[][] gameTiles;
+  protected int maxTile;
+  protected int score = 0;
+
 
   public Model() {
     resetGameTiles();
@@ -43,9 +49,50 @@ public class Model {
         gameTiles[i][j] = new Tile();
       }
     }
-    // maxTile = 0;
+    maxTile = 0;
     addTile();
     addTile();
+  }
+
+  private boolean compressTiles(Tile[] tiles){
+    AtomicBoolean marker = new AtomicBoolean();
+    Arrays.sort(tiles, new Comparator<Tile>() {
+      @Override
+      public int compare(Tile o1, Tile o2) {
+        if(o1.value == 0 & o2.value == 0){
+          return 0;
+        }
+        else if(o1.value == 0 | o2.value == 0){
+          int result = o2.value - o1.value;
+          if(result < 0) marker.set(true);
+          return result;
+        }
+        else return 0;
+      }
+    });
+
+    return marker.get();
+  }
+
+  private boolean mergeTiles(Tile[] tiles){
+    AtomicBoolean marker = new AtomicBoolean();
+    compressTiles(tiles);
+    Arrays.sort(tiles, new Comparator<Tile>() {
+      @Override
+      public int compare(Tile o1, Tile o2) {
+        if (o1.value == o2.value & o1.value != 0) {
+          o1.value *= 2;
+          o2.value = 0;
+          compressTiles(tiles);
+          score += o1.value;
+          if (maxTile < o1.value) maxTile = o1.value;
+          marker.set(true);
+          return 0;
+        }
+        else return 0;
+      }
+    });
+    return marker.get();
   }
 
 }
