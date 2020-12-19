ed25b79d080fe8c6519cb4c4ea1e5108113e6de3
Ponser2000 <ponser2000@gmail.com>
2020-12-19 18:47:12 +0300

    - 2048 (2)

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Main.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Main.java
@@ -5,7 +5,7 @@ package com.javarush.task.task35.task3513;
  * @project JavaRushTasks/com.javarush.task.task35.task3513
  */
 public class Main {
-
+  
   public static void main(String[] args) {}
 
 }
