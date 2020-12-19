a601e95e9da10e108d245d2d13ae3ab71f7c088d
Ponser2000 <ponser2000@gmail.com>
2020-12-19 18:40:11 +0300

    - 2048 (1)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task35.task3513;
+
+/**
+ * @author Sergey Ponomarev on 19.12.2020
+ * @project JavaRushTasks/com.javarush.task.task35.task3513
+ */
+public class Controller {
+
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Main.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task35.task3513;
+
+/**
+ * @author Sergey Ponomarev on 19.12.2020
+ * @project JavaRushTasks/com.javarush.task.task35.task3513
+ */
+public class Main {
+
+  public static void main(String[] args) {}
+
+}
