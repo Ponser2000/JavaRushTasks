73285b8efa2a667ebae807b043efd281f4edb3d6
Ponser2000 <ponser2000@gmail.com>
2020-12-21 15:34:22 +0300

    - Tetris (1)

---
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task22.task2213;
+
+/**
+ * @author Sergey Ponomarev on 21.12.2020
+ * @project JavaRushTasks/com.javarush.task.task22.task2213
+ */
+public class Figure {
+
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task22.task2213;
+
+/**
+ * @author Sergey Ponomarev on 21.12.2020
+ * @project JavaRushTasks/com.javarush.task.task22.task2213
+ */
+public class Tetris {
+
+  public static void main(String... args){}
+
+}
