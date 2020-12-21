1e63bd59498fb7ed18e2ae8dc488bd6407f04323
Ponser2000 <ponser2000@gmail.com>
2020-12-21 15:48:57 +0300

    - Tetris (6)

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
@@ -5,5 +5,28 @@ package com.javarush.task.task22.task2213;
  * @project JavaRushTasks/com.javarush.task.task22.task2213
  */
 public class Field {
+  //ширина и высота
+  private int width;
+  private int height;
 
+  //матрица поля: 1 - клетка занята, 0 - свободна
+  private int[][] matrix;
+
+  public Field(int width, int height) {
+    this.width = width;
+    this.height = height;
+    matrix = new int[height][width];
+  }
+
+  public int getWidth() {
+    return width;
+  }
+
+  public int getHeight() {
+    return height;
+  }
+
+  public int[][] getMatrix() {
+    return matrix;
+  }
 }
