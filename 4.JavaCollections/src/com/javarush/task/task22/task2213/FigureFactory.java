1f05f056380bbe109969efac70983a6987531c4e
Ponser2000 <ponser2000@gmail.com>
2020-12-21 15:58:28 +0300

    - Tetris (11)

---
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/FigureFactory.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task22.task2213;
+
+/**
+ * @author Sergey Ponomarev on 21.12.2020
+ * @project JavaRushTasks/com.javarush.task.task22.task2213
+ */
+public class FigureFactory {
+
+  /**
+   * Метод выбирает случайный шаблон и создает с ним новую фигурку.
+   */
+  public static Figure createRandomFigure(int x, int y) {
+    //int index = (int) (Math.random() * 6);
+    //return new Figure(x, y, BRICKS[index]);
+    return null;
+  }
+
+
+}
