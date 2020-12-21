bdb8ad2faf8216134cb3a56832c16b2543fffafa
Ponser2000 <ponser2000@gmail.com>
2020-12-21 14:54:25 +0300

    - Added problem solution / task3205

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3205/CustomInvocationHandler.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task32.task3205;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * @author Sergey Ponomarev on 21.12.2020
+ * @project JavaRushTasks/com.javarush.task.task32.task3205
+ */
+public class CustomInvocationHandler implements InvocationHandler {
+  private SomeInterfaceWithMethods object;
+
+  public CustomInvocationHandler(SomeInterfaceWithMethods object) {
+    this.object = object;
+  }
+
+  @Override
+  public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+    System.out.println(method.getName()+" in");
+    if(method.getReturnType().getSimpleName().equals("String")) {
+      String result = (String)method.invoke(object, args);
+      System.out.println(method.getName() + " out");
+      return result;
+    }
+    else {
+      method.invoke(object, args);
+      System.out.println(method.getName()+" out");
+      return null;
+    }
+
+  }
+
+}
