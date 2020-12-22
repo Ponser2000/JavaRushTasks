eaaae994db42de2385b1b8fcaed7cc1c24e30370
Ponser2000 <ponser2000@gmail.com>
2020-12-22 13:34:40 +0300

    - Space (13)

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
@@ -11,15 +11,19 @@ public class SpaceShip extends BaseObject{
     super(x, y, 3);
   }
 
-  public void draw() {
-
-  }
+  public void draw(Canvas canvas){}
 
   @Override
   public void move() {
-
+    x += dx;
+    checkBorders(radius, Space.game.getWidth() - radius + 1, 1, Space.game.getHeight() + 1);
   }
 
   public void moveLeft() { dx = -1;}
   public void moveRight() {dx = 1;}
+
+  public void fire(){
+    Space.game.getRockets().add(new Rocket(x - 2, y));
+    Space.game.getRockets().add(new Rocket(x + 2, y));
+  }
 }
