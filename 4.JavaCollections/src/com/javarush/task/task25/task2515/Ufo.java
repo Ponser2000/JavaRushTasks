9e34eac7919573c2ba4e67f30dd41d8dc37704b1
Ponser2000 <ponser2000@gmail.com>
2020-12-22 12:51:12 +0300

    - Space (1)

---
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Ufo.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task25.task2515;
+
+/**
+ * @author Sergey Ponomarev on 22.12.2020
+ * @project JavaRushTasks/com.javarush.task.task25.task2515
+ */
+public class Ufo {
+
+}
