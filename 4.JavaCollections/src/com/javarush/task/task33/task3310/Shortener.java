7015b907f5471032c13c13aee1dbe74d95bee953
Ponser2000 <ponser2000@gmail.com>
2021-01-03 20:01:00 +0300

    - Shortener (3)

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
@@ -1,13 +1,30 @@
 package com.javarush.task.task33.task3310;
 
+import com.javarush.task.task33.task3310.strategy.StorageStrategy;
+
 /**
  * @author Sergey Ponomarev on 03.01.2021
  * @project JavaRushTasks/com.javarush.task.task33.task3310
  */
 public class Shortener {
+  private Long lastId = 0L;
+  private StorageStrategy storageStrategy;
 
-  public Long getId(String string){ return null;}
+  public Shortener(StorageStrategy storageStrategy) {
+    this.storageStrategy = storageStrategy;
+  }
 
-  public String getString(Long id) {return null;}
+  public synchronized Long getId(String string){
+    if (storageStrategy.containsValue(string)) {
+      return storageStrategy.getKey(string);
+    }
+    lastId++;
+    storageStrategy.put(lastId,string);
+    return lastId;
+  }
+
+  public synchronized String getString(Long id) {
+    return storageStrategy.getValue(id);
+  }
 
 }
