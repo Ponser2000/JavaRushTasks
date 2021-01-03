038e15a16eeca126376d8b1959ca9db522ea9b26
Ponser2000 <ponser2000@gmail.com>
2021-01-03 19:45:00 +0300

    - Shortener (1)

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task33.task3310;
+
+/**
+ * @author Sergey Ponomarev on 03.01.2021
+ * @project JavaRushTasks/com.javarush.task.task33.task3310
+ */
+public class Shortener {
+
+  public Long getId(String string){ return null;}
+
+  public String getString(Long id) {return null;}
+
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task33.task3310;
+
+/**
+ * @author Sergey Ponomarev on 03.01.2021
+ * @project JavaRushTasks/com.javarush.task.task33.task3310
+ */
+public class Solution {
+  public static void main(String[] args) {
+
+  }
+}
