f2d64c1e657ea1a39269d2a49755bc5e1cc540c1
Ponser2000 <ponser2000@gmail.com>
2021-01-04 21:54:44 +0300

    - Added problem solution / task3709

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3709/connectors/SecurityProxyConnector.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task37.task3709.connectors;
+
+import com.javarush.task.task37.task3709.security.SecurityChecker;
+import com.javarush.task.task37.task3709.security.SecurityCheckerImpl;
+
+/**
+ * @author Sergey Ponomarev on 04.01.2021
+ * @project JavaRushTasks/com.javarush.task.task37.task3709.connectors
+ */
+public class SecurityProxyConnector implements Connector {
+  private SimpleConnector simpleConnector;
+  private SecurityChecker securityChecker;
+
+  public SecurityProxyConnector(String resourceString){
+    this.simpleConnector = new SimpleConnector(resourceString);
+    this.securityChecker = new SecurityCheckerImpl();
+  }
+
+  @Override
+  public void connect() {
+    if (securityChecker.performSecurityCheck()) {
+      simpleConnector.connect();
+    }
+  }
+}
