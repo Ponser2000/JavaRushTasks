49112150c1aa8eae52cf559d2c97351999f3c329
Ponser2000 <ponser2000@gmail.com>
2021-01-05 23:28:05 +0300

    - Aggregator (1)

---
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task28.task2810;
+
+/**
+ * @author Sergey Ponomarev on 05.01.2021
+ * @project JavaRushTasks/com.javarush.task.task28.task2810
+ */
+public class Aggregator {
+
+  public static void main(String[] args) {
+
+  }
+
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/Strategy.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task28.task2810.model;
+
+/**
+ * @author Sergey Ponomarev on 05.01.2021
+ * @project JavaRushTasks/com.javarush.task.task28.task2810.model
+ */
+public interface Strategy {
+
+}
