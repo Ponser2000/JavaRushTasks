0d922ede7a2241c50941a3e6084d594c3e15345f
Ponser2000 <ponser2000@gmail.com>
2021-01-06 11:02:32 +0300

    - Aggregator (15)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
@@ -6,6 +6,10 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 /**
  * @author Sergey Ponomarev on 06.01.2021
@@ -37,7 +41,46 @@ public class HtmlView implements View {
   }
 
   private String getUpdatedFileContent(List<Vacancy> vacancies) {
-    return null;
+    try {
+      Document doc = getDocument();
+      Elements templateHidden = doc.getElementsByClass("template");
+      Element template = templateHidden.clone().removeAttr("style").removeClass("template").get(0);
+
+      //remove all prev vacancies
+      Elements prevVacancies = doc.getElementsByClass("vacancy");
+
+      for (Element redundant : prevVacancies) {
+        if (!redundant.hasClass("template")) {
+          redundant.remove();
+        }
+      }
+
+      //add new vacancies
+      for (Vacancy vacancy : vacancies) {
+        Element vacancyElement = template.clone();
+
+        Element vacancyLink = vacancyElement.getElementsByAttribute("href").get(0);
+        vacancyLink.appendText(vacancy.getTitle());
+        vacancyLink.attr("href", vacancy.getUrl());
+        Element city = vacancyElement.getElementsByClass("city").get(0);
+        city.appendText(vacancy.getCity());
+        Element companyName = vacancyElement.getElementsByClass("companyName").get(0);
+        companyName.appendText(vacancy.getCompanyName());
+        Elements salaryEls = vacancyElement.getElementsByClass("salary");
+        Element salary = salaryEls.get(0);
+        salary.appendText(vacancy.getSalary());
+
+        templateHidden.before(vacancyElement.outerHtml());
+      }
+      return doc.html();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return "Some exception occurred";
+  }
+
+  protected Document getDocument() throws IOException {
+    return Jsoup.parse(new File(filePath), "UTF-8");
   }
 
   private void updateFile(String content) {
