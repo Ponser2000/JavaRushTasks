77dcb008f43252deb3a97672613fce70f016fc5c
Ponser2000 <ponser2000@gmail.com>
2021-01-06 11:09:21 +0300

    - Aggregator (16)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task28.task2810;
 
 import com.javarush.task.task28.task2810.model.HHStrategy;
+import com.javarush.task.task28.task2810.model.HabrCareerStrategy;
 import com.javarush.task.task28.task2810.model.Model;
 import com.javarush.task.task28.task2810.model.Provider;
 import com.javarush.task.task28.task2810.view.HtmlView;
@@ -14,10 +15,10 @@ import java.io.IOException;
 public class Aggregator {
 
   public static void main(String[] args) {
-
     HtmlView view = new HtmlView();
-    Model model = new Model(view,new Provider(new HHStrategy()));
+    Model model = new Model(view, new Provider(new HHStrategy()), new Provider(new HabrCareerStrategy()));
     Controller controller = new Controller(model);
+
     view.setController(controller);
 
     view.userCitySelectEmulationMethod();
