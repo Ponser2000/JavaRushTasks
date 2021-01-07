8d4b3f272144e38b43e23150836e250a54259057
Ponser2000 <ponser2000@gmail.com>
2021-01-07 12:27:03 +0300

    - Added problem solution / task 3906

---
+++ 4.JavaCollections/src/com/javarush/task/task39/task3906/Switchable.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task39.task3906;
+
+/**
+ * @author Sergey Ponomarev on 07.01.2021
+ * @project JavaRushTasks/com.javarush.task.task39.task3906
+ */
+public interface Switchable {
+  public boolean isOn();
+  public void turnOff();
+  public void turnOn();
+}
