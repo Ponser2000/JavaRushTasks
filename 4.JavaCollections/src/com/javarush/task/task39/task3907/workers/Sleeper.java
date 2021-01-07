7f60790f5af7edd898c491049cf47f05508255d0
Ponser2000 <ponser2000@gmail.com>
2021-01-07 12:30:48 +0300

    - Added problem solution / task 3907

---
+++ 4.JavaCollections/src/com/javarush/task/task39/task3907/workers/Sleeper.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task39.task3907.workers;
+
+/**
+ * @author Sergey Ponomarev on 07.01.2021
+ * @project JavaRushTasks/com.javarush.task.task39.task3907.workers
+ */
+public interface Sleeper {
+  void sleep();
+}
