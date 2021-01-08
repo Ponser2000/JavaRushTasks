659d0a3b0dcdafd3c322c2fc81bfdec38e70882c
Ponser2000 <ponser2000@gmail.com>
2021-01-08 18:55:21 +0300

    - CashMachine (4)

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/CashMachine.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/CashMachine.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task26.task2613;
 
+import java.util.Locale;
+
 /**
  * @author Sergey Ponomarev on 07.01.2021
  * @project JavaRushTasks/com.javarush.task.task26.task2613
@@ -7,6 +9,14 @@ package com.javarush.task.task26.task2613;
 public class CashMachine {
 
   public static void main(String[] args) {
+    Locale.setDefault(Locale.ENGLISH);
+    String currencyCode = ConsoleHelper.askCurrencyCode();
+    String[] digits = ConsoleHelper.getValidTwoDigits(currencyCode);
 
+    int denomination = Integer.parseInt(digits[0]);
+    int count = Integer.parseInt(digits[1]);
+
+    CurrencyManipulator currencyManipulator = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(currencyCode);
+    currencyManipulator.addAmount(denomination,count);
   }
 }
--- 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
@@ -3,6 +3,8 @@ package com.javarush.task.task26.task2613;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Sergey Ponomarev on 07.01.2021
@@ -25,4 +27,46 @@ public class ConsoleHelper {
     return null;
   }
 
+  public static String askCurrencyCode(){
+    writeMessage("Введите код валюды (XXX): ");
+    Pattern p = Pattern.compile("^[A-zА-яЁё]+$");
+
+    while(true) {
+      String code = readString();
+      Matcher m = p.matcher(code);
+      if (code.length() != 3 || ! m.find()) {
+        writeMessage("Повторите ввод:");
+        continue;
+      }
+      return code.toUpperCase();
+    }
+  }
+
+  public static String[] getValidTwoDigits(String currencyCode){
+    writeMessage("Введите через пробел номинал и количество купюр:");
+    String[] dengi;
+    while(true) {
+      String inputNominalAndCount = readString();
+      inputNominalAndCount = inputNominalAndCount.replaceAll("\\s+"," ");
+      dengi = inputNominalAndCount.split(" ");
+      if (dengi.length != 2) {
+        writeMessage("Повторите ввод:");
+        continue;
+      }
+      Pattern p = Pattern.compile("^\\d+$");
+      Matcher m1 = p.matcher(dengi[0]);
+      Matcher m2 = p.matcher(dengi[1]);
+      if( ! (m1.find() && m2.find()) ) {
+        writeMessage("Повторите ввод:");
+        continue;
+      }
+      if( Integer.parseInt(dengi[0]) < 1 && Integer.parseInt(dengi[1]) < 1) {
+        writeMessage("Повторите ввод:");
+        continue;
+      }
+
+      return dengi;
+    }
+
+  }
 }
