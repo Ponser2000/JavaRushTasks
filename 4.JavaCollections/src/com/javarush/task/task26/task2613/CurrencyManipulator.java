b2f2a415d767175c31ffef8f04ed64e311e39d4c
Ponser2000 <ponser2000@gmail.com>
2021-01-08 22:04:13 +0300

    - CashMachine (10)

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/CurrencyManipulator.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/CurrencyManipulator.java
@@ -1,5 +1,10 @@
 package com.javarush.task.task26.task2613;
 
+import com.javarush.task.task26.task2613.exception.NotEnoughMoneyException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -8,6 +13,7 @@ import java.util.TreeMap;
  * @project JavaRushTasks/com.javarush.task.task26.task2613.exception
  */
 public class CurrencyManipulator {
+
   private String currencyCode;
   private Map<Integer, Integer> denominations;
 
@@ -20,19 +26,67 @@ public class CurrencyManipulator {
     return currencyCode;
   }
 
-  public void addAmount(int denomination, int count){
+  public void addAmount(int denomination, int count) {
     if (denominations.containsKey(denomination)) {
-      denominations.put(denomination,denominations.get(denomination) + count);
+      denominations.put(denomination, denominations.get(denomination) + count);
     } else {
       denominations.put(denomination, count);
     }
   }
 
-  public int getTotalAmount(){
+  public int getTotalAmount() {
     return denominations.keySet().stream().mapToInt(key -> denominations.get(key) * key).sum();
   }
 
-  public boolean hasMoney(){
-    return denominations.size()>0;
+  public boolean hasMoney() {
+    return !denominations.isEmpty();
+  }
+
+  public boolean isAmountAvailable(int expectedAmount) {
+    return expectedAmount <= getTotalAmount();
+  }
+
+  public Map<Integer, Integer> withdrawAmount(int expectedAmount) throws NotEnoughMoneyException {
+    int sum = expectedAmount;
+    HashMap<Integer, Integer> copyDenominations = new HashMap<>(denominations);
+
+    ArrayList<Integer> keys = new ArrayList<>(this.denominations.keySet());
+
+    Collections.sort(keys);
+    Collections.reverse(keys);
+
+    TreeMap<Integer, Integer> resultMap = new TreeMap<>(new Comparator<Integer>() {
+      @Override
+      public int compare(Integer o1, Integer o2) {
+        return o2.compareTo(o1);
+      }
+    });
+
+    for (Integer denomination : keys) {
+      final int key = denomination;
+      int value = copyDenominations.get(key);
+      while (true) {
+        if (sum < key || value == 0) {
+          copyDenominations.put(key, value);
+          break;
+        }
+        sum -= key;
+        value--;
+
+        if (resultMap.containsKey(key)) {
+          resultMap.put(key, resultMap.get(key) + 1);
+        } else {
+          resultMap.put(key, 1);
+        }
+      }
+    }
+
+    if (sum > 0) {
+      throw new NotEnoughMoneyException();
+    } else {
+      this.denominations.clear();
+      this.denominations.putAll(copyDenominations);
+    }
+    return resultMap;
   }
 }
--- 4.JavaCollections/src/com/javarush/task/task26/task2613/command/WithdrawCommand.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/command/WithdrawCommand.java
@@ -1,5 +1,12 @@
 package com.javarush.task.task26.task2613.command;
 
+import com.javarush.task.task26.task2613.ConsoleHelper;
+import com.javarush.task.task26.task2613.CurrencyManipulator;
+import com.javarush.task.task26.task2613.CurrencyManipulatorFactory;
+import com.javarush.task.task26.task2613.exception.InterruptOperationException;
+import com.javarush.task.task26.task2613.exception.NotEnoughMoneyException;
+import java.util.Map;
+
 /**
  * @author Sergey Ponomarev on 08.01.2021
  * @project JavaRushTasks/com.javarush.task.task26.task2613.command
@@ -7,7 +14,40 @@ package com.javarush.task.task26.task2613.command;
 class WithdrawCommand implements Command{
 
   @Override
-  public void execute() {
+  public void execute() throws InterruptOperationException {
+    ConsoleHelper.writeMessage("Withdrawing...");
 
+    String currencyCode = ConsoleHelper.askCurrencyCode();
+    CurrencyManipulator manipulator = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(currencyCode);
+
+    while (true) {
+      try {
+        ConsoleHelper.writeMessage("Пожалуйста, введите сумму для снятия.");
+        String s = ConsoleHelper.readString();
+        if (s == null) {
+          ConsoleHelper.writeMessage("Пожалуйста, введи правильную сумму.");
+        } else {
+          try {
+            int amount = Integer.parseInt(s);
+            boolean isAmountAvailable = manipulator.isAmountAvailable(amount);
+            if (isAmountAvailable) {
+              Map<Integer, Integer> denominations = manipulator.withdrawAmount(amount);
+              for (Integer item : denominations.keySet()) {
+                ConsoleHelper.writeMessage("\t" + item + " - " + denominations.get(item));
+              }
+
+              ConsoleHelper.writeMessage(String.format("%d %s снятие прошло успешно", amount, currencyCode));
+              break;
+            } else {
+              ConsoleHelper.writeMessage("На вашем счете недостаточно средств, повторите ввод");
+            }
+          } catch (NumberFormatException e) {
+            ConsoleHelper.writeMessage("Пожалуйста, введи правильную сумму.");
+          }
+        }
+      } catch (NotEnoughMoneyException e) {
+        ConsoleHelper.writeMessage("На вашем счете недостаточно средств.");
+      }
+    }
   }
 }
