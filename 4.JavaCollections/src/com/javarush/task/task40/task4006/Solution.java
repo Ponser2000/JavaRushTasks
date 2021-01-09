7f77146b74277b40aaf9ce629586e13faa1f1c32
Ponser2000 <ponser2000@gmail.com>
2021-01-09 13:09:02 +0300

    - Added problem solution / task4006

---
+++ 4.JavaCollections/src/com/javarush/task/task40/task4006/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task40.task4006;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.net.HttpURLConnection;
+import java.net.Socket;
+import java.net.URL;
+
+/* 
+Отправка GET-запроса через сокет
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        getSite(new URL("http://javarush.ru/social.html"));
+    }
+
+    public static void getSite(URL url) {
+        try {
+            Socket socket = new Socket(url.getHost(), url.getDefaultPort());
+
+            StringBuilder sb = new StringBuilder("GET ").append(url.getFile()).append(" HTTP/1.1\r\n")
+                .append("Host: ").append(url.getHost()).append("\r\n")
+                .append("User-Agent: Mozilla/5.0 (Windows NT 6.1; rv:18.0) Gecko/20100101 Firefox/18.0\r\n")
+                .append("Accept: text/html\r\n").append("Connection: close\r\n\r\n");
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            writer.write(sb.toString());
+            writer.flush();
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String responseLine;
+
+            while ((responseLine = bufferedReader.readLine()) != null) {
+                System.out.println(responseLine);
+            }
+            bufferedReader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
