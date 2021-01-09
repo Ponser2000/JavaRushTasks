5bcb95c003c193f76f386c65329adc7e93b2abd8
Ponser2000 <ponser2000@gmail.com>
2021-01-09 18:57:41 +0300

    - Sokoban (11)

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task34.task3410.model;
 
 import com.javarush.task.task34.task3410.controller.EventListener;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
 
 /**
  * @author Sergey Ponomarev on 09.01.2021
@@ -9,9 +11,36 @@ import com.javarush.task.task34.task3410.controller.EventListener;
 public class Model {
   public static final int FIELD_CELL_SIZE = 20;
   private EventListener eventListener;
+  private GameObjects gameObjects;
+  private int currentLevel = 1;
+  private LevelLoader levelLoader;
+
+  public Model() {
+    try {
+      levelLoader = new LevelLoader(Paths.get(getClass().getResource("../res/levels.txt").toURI()));
+    } catch (URISyntaxException e) {
+    }
+  }
 
   public void setEventListener(EventListener eventListener){
     this.eventListener = eventListener;
   }
 
+
+  public void restart() {
+    restartLevel(currentLevel);
+  }
+
+  public void startNextLevel() {
+    currentLevel++;
+    restartLevel(currentLevel);
+  }
+
+  public void restartLevel(int level) {
+    gameObjects = levelLoader.getLevel(level);
+  }
+
+  public GameObjects getGameObjects() {
+    return gameObjects;
+  }
 }
