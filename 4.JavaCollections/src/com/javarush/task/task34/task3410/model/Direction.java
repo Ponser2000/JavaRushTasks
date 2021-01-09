a33a5c2f05dcd3ef8918186669056068a62a87a2
Ponser2000 <ponser2000@gmail.com>
2021-01-09 18:18:34 +0300

    - Sokoban (4)

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Direction.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task34.task3410.model;
+
+/**
+ * @author Sergey Ponomarev on 09.01.2021
+ * @project JavaRushTasks/com.javarush.task.task34.task3410.model
+ */
+public enum Direction {
+  LEFT, RIGHT, UP , DOWN
+}
