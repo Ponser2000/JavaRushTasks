97d068b4e36e72b8b024bd295689a312c60f0966
Ponser2000 <ponser2000@gmail.com>
2021-01-11 15:34:33 +0300

    - Snake (4)

---
+++ 8.Games/src/com/javarush/games/snake/Apple.java
@@ -0,0 +1,20 @@
+package com.javarush.games.snake;
+
+import com.javarush.engine.cell.*;
+
+/**
+ * @author Sergey Ponomarev on 11.01.2021
+ * @project JavaRushTasks/com.javarush.games.snake
+ */
+public class Apple extends GameObject{
+  private static final String APPLE_SIGN = "\uD83C\uDF4E";
+
+  public Apple(int x, int y) {
+    super(x, y);
+  }
+
+  public void draw(Game game){
+    game.setCellValueEx(x, y, Color.NONE, APPLE_SIGN, Color.GREEN, 75);
+  }
+
+}
