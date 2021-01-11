f907401ce54aefbe75a8f1069a56b4299fab8db8
Ponser2000 <ponser2000@gmail.com>
2021-01-11 15:28:21 +0300

    - Snake (3)

---
+++ 8.Games/src/com/javarush/games/snake/GameObject.java
@@ -0,0 +1,18 @@
+package com.javarush.games.snake;
+
+/**
+ * @author Sergey Ponomarev on 11.01.2021
+ * @project JavaRushTasks/com.javarush.games.snake
+ */
+public class GameObject {
+  public int x;
+  public int y;
+
+  public GameObject(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+
+
+}
