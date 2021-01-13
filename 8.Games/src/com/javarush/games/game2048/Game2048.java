d93a0add5e3d5e8242c63dce159cd13f6654c669
Ponser2000 <ponser2000@gmail.com>
2021-01-13 15:50:51 +0300

    - Game2048 (7)

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -46,21 +46,34 @@ public class Game2048 extends Game {
   }
 
   private Color getColorByValue(int value){
-    return switch (value) {
-      case 0 -> Color.WHITE;
-      case 2 -> Color.PLUM;
-      case 4 -> Color.SLATEBLUE;
-      case 8 -> Color.DODGERBLUE;
-      case 16 -> Color.DARKTURQUOISE;
-      case 32 -> Color.MEDIUMSEAGREEN;
-      case 64 -> Color.LIMEGREEN;
-      case 128 -> Color.DARKORANGE;
-      case 256 -> Color.SALMON;
-      case 512 -> Color.ORANGERED;
-      case 1024 -> Color.DEEPPINK;
-      case 2048 -> Color.MEDIUMVIOLETRED;
-      default -> Color.NONE;
-    };
+    switch (value) {
+      case 0:
+        return Color.WHITE;
+      case 2:
+        return Color.PLUM;
+      case 4:
+        return Color.SLATEBLUE;
+      case 8:
+        return Color.DODGERBLUE;
+      case 16:
+        return Color.DARKTURQUOISE;
+      case 32:
+        return Color.MEDIUMSEAGREEN;
+      case 64:
+        return Color.LIMEGREEN;
+      case 128:
+        return Color.DARKORANGE;
+      case 256:
+        return Color.SALMON;
+      case 512:
+        return Color.ORANGERED;
+      case 1024:
+        return Color.DEEPPINK;
+      case 2048:
+        return Color.MEDIUMVIOLETRED;
+      default:
+        return Color.NONE;
+    }
   }
 
   private void setCellColoredNumber(int x, int y, int value){
@@ -68,5 +81,21 @@ public class Game2048 extends Game {
     String msg = value > 0 ? String.valueOf(value) : "";
     setCellValueEx(x, y, color, msg);
 
+  }
+
+  private boolean compressRow(int[] row){
+    int insertPosition = 0;
+    boolean result = false;
+    for (int x = 0; x < SIDE; x++) {
+      if (row[x] > 0) {
+        if (x != insertPosition) {
+          row[insertPosition] = row[x];
+          row[x] = 0;
+          result = true;
+        }
+        insertPosition++;
+      }
+    }
+    return result;
   }
 }
