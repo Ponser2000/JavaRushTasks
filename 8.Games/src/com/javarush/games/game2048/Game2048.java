8a04fca2cfd9f1d8c2c35e582593fae14eba9797
Ponser2000 <ponser2000@gmail.com>
2021-01-13 14:49:59 +0300

    - Game2048 (3)

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -1,6 +1,6 @@
 package com.javarush.games.game2048;
 
-import com.javarush.engine.cell.*;
+import com.javarush.engine.cell.Game;
 
 /**
  * @author Sergey Ponomarev on 13.01.2021
@@ -10,8 +10,16 @@ public class Game2048 extends Game {
 
   private static final int SIDE = 4;
 
+  private int[][] gameField = new int[SIDE][SIDE];
+
+
   @Override
   public void initialize() {
     setScreenSize(SIDE, SIDE);
+    createGame();
+  }
+
+  private void createGame() {
+
   }
 }
