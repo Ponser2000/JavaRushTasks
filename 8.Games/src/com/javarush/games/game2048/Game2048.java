09434eac58c903a9127aefba8b7897df051eea00
Ponser2000 <ponser2000@gmail.com>
2021-01-13 14:45:44 +0300

    - Game2048 (1)

---
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -0,0 +1,11 @@
+package com.javarush.games.game2048;
+
+import com.javarush.engine.cell.*;
+
+/**
+ * @author Sergey Ponomarev on 13.01.2021
+ * @project JavaRushTasks/com.javarush.games.game2048
+ */
+public class Game2048 extends Game {
+
+}
