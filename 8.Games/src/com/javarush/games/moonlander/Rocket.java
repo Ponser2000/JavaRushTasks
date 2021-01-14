ca86fcd3359790cdf35764e80923167c1d796899
Ponser2000 <ponser2000@gmail.com>
2021-01-14 12:42:06 +0300

    - MoonLander (4)

---
+++ 8.Games/src/com/javarush/games/moonlander/Rocket.java
@@ -0,0 +1,12 @@
+package com.javarush.games.moonlander;
+
+/**
+ * @author Sergey Ponomarev on 14.01.2021
+ * @project JavaRushTasks/com.javarush.games.moonlander
+ */
+public class Rocket extends GameObject{
+
+  public Rocket(double x, double y) {
+    super(x, y, ShapeMatrix.ROCKET);
+  }
+}
