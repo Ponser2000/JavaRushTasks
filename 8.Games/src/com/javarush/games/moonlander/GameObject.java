10876353bad2c1cc117916788e4f8755bf381eec
Ponser2000 <ponser2000@gmail.com>
2021-01-14 12:35:25 +0300

    - MoonLander (3)

---
+++ 8.Games/src/com/javarush/games/moonlander/GameObject.java
@@ -0,0 +1,15 @@
+package com.javarush.games.moonlander;
+
+/**
+ * @author Sergey Ponomarev on 14.01.2021
+ * @project JavaRushTasks/com.javarush.games.moonlander
+ */
+public class GameObject {
+  public double x;
+  public double y;
+
+  public GameObject(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+}
