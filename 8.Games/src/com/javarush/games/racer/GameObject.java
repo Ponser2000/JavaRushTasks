8f91839ec84a7388bd2d87ea726b22e0b260e196
Ponser2000 <ponser2000@gmail.com>
2021-03-22 14:52:59 +0300

    - Racer (5)

---
+++ 8.Games/src/com/javarush/games/racer/GameObject.java
@@ -0,0 +1,33 @@
+package com.javarush.games.racer;
+
+import com.javarush.engine.cell.*;
+
+/**
+ * @author Sergey Ponomarev on 22.03.2021
+ * @project JavaRushTasks/com.javarush.games.racer
+ */
+public class GameObject {
+  public int x;
+  public int y;
+
+  public int[][] matrix;
+
+  public int width;
+  public int height;
+
+  public GameObject(int x, int y, int[][] matrix) {
+    this.x = x;
+    this.y = y;
+    this.matrix = matrix;
+    this.width = matrix[0].length;
+    this.height = matrix.length;
+  }
+
+  public  void draw(Game game){
+    for(int i=0; i<width; i++) {
+      for(int j=0; j<height; j++) {
+        game.setCellColor(this.x + i,this.y + j,Color.values()[matrix[j][i]]);
+      }
+    }
+  }
+}
