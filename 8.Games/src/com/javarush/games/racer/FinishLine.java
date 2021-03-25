3506852e2235c46c7107b7fdb20eff02903e9d0c
Ponser2000 <ponser2000@gmail.com>
2021-03-25 13:09:22 +0300

    - Race (22)

---
+++ 8.Games/src/com/javarush/games/racer/FinishLine.java
@@ -0,0 +1,30 @@
+package com.javarush.games.racer;
+
+import com.javarush.games.racer.GameObject;
+import com.javarush.games.racer.RacerGame;
+import com.javarush.games.racer.ShapeMatrix;
+
+/**
+ * @author Sergey Ponomarev on 25.03.2021
+ * @project JavaRushTasks/com.javarush.games.racer.road
+ */
+public class FinishLine extends GameObject {
+
+  private boolean isVisible = false;
+
+  public FinishLine() {
+    super(RacerGame.ROADSIDE_WIDTH, -1 * ShapeMatrix.FINISH_LINE.length, ShapeMatrix.FINISH_LINE);
+  }
+
+  public void show(){
+    isVisible = true;
+  }
+
+  public void move(int boost){
+    if (!isVisible)
+      return;
+
+    y += boost;
+  }
+
+}
